30946818e48dbe486f5d824da92fd1b5dfeec968
Seunghwan <ysp1307@naver.com>
2021-07-24 23:57:21 +0900

    navbar clear, profile set

--- src/Login.js
+++ src/Login.js
@@ -4,11 +4,25 @@ import "./Login.scss";
 function Login(props) {
     return (
         <div>
-            <div className="background">
-            로f그인 페이지임다.
-
+            <div className="Navbar">
+                <img src="img/logo.png" width="100px" onClick={()=>{}}></img>
             </div>
+            <h1 className="profile_header"> 넷플릭스를 시청할 프로필을 선택하세요.</h1>
+        <ul className="choose_profile">
+            <li className="profile">
+                <img src="img/profile.png" width="100px"></img>
+            </li>
+            <li className="profile">
+                <img src="img/profile2.png" width="90px"></img>
+            </li>
+            <li className="profile">
+                <img src="img/profile3.png" width="100px"></img>
+            </li>
             
+
+        </ul>        
+        
+        
         </div>
     )
 }
