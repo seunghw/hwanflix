c965b5806a3ca221e276a19fac81831ea18aa69a
Seunghwan <ysp1307@naver.com>
2021-08-08 22:56:17 +0900

    영화 메인페이지 데이터 불러오기 및 gridCards, MainImage 작성 완료

--- src/Movies.js
+++ src/Movies.js
@@ -1,9 +1,61 @@
-import "./Movies.scss"
+import React, { useEffect, useState } from "react";
+import "./Movies.scss";
+import { API_URL, API_KEY, IMAGE_BASE_URL } from "./Config";
+import MainImage from "./MainImage";
+import GridCards from "./GridCards";
+import { Row } from "antd";
 
-function movies(props) {
-    return (
-        <div className="main">준비중입니다.</div>
-    )
+function Movies(props) {
+  const [Movies, setMovies] = useState([]);
+  const [MainMovieImage, setMainMovieImage] = useState(null);
+
+  useEffect(() => {
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=ko-US&page=1`;
+
+    fetch(endpoint)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response.results);
+        setMovies([...Movies, ...response.results]);
+        setMainMovieImage(response.results[1]);
+      });
+  }, []);
+
+  return (
+    <div style={{ width: "100%", margin: "0" }}>
+      {MainMovieImage && (
+        <MainImage
+          image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
+          title={MainMovieImage.original_title}
+          text={MainMovieImage.overview}
+        />
+      )}
+      <div style={{ width: "85%", margin: "1rem auto" }}>
+        <h2 style={{ color: "white" }}>Movies by lastest</h2>
+
+        <Row gutter={[16, 16]}>
+          {Movies &&
+            Movies.map((movie, index) => (
+              <React.Fragment key={index}>
+                <GridCards
+                  image={
+                    movie.poster_path
+                      ? `${IMAGE_BASE_URL}w500${movie.poster_path}`
+                      : null
+                  }
+                  movieId={movie.id}
+                  movieName={movie.original_title}
+                />
+              </React.Fragment>
+            ))}
+        </Row>
+      </div>
+
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <button> Load More</button>
+      </div>
+    </div>
+  );
 }
 
-export default movies;
+export default Movies;
