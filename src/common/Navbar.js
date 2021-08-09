e8fa4986580ec287c3781f057d07ebda3360622b
Seunghwan <ysp1307@naver.com>
2021-08-09 23:06:24 +0900

    navbar 컴포넌트화

---
+++ src/common/Navbar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+function Navbar() {
+  let history = useHistory();
+
+  return (
+    <div>
+      <div className="Navbar">
+        <img
+          src="img/logo.png"
+          width="100px"
+          onClick={() => {
+            history.push("/");
+          }}
+        ></img>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
