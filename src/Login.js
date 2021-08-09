de92525037498f609951ab087f03a562a2ad8f77
Seunghwan <ysp1307@naver.com>
2021-08-09 23:05:30 +0900

    로그인 페이지 경로 수정

--- src/Login.js
+++ src/Login.js
@@ -1,38 +1,50 @@
-import { useHistory } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
 import "./Login.scss";
 
 function Login(props) {
+  let history = useHistory();
 
-    let history = useHistory();
+  return (
+    <div>
+      <h1 className="profile_header">
+        {" "}
+        넷플릭스를 시청할 프로필을 선택하세요.
+      </h1>
 
-    return (
-        <div>
-        
-            <h1 className="profile_header"> 넷플릭스를 시청할 프로필을 선택하세요.</h1>
-            
-            <ul className="choose_profile">
-                {props.data.map((a, i) => {
-                    return <Profile data={props.data[i]} i={i} key={i}/>
-                })}
-            </ul>  
-            <div className="profile_set">
-                <button className="profile_button" onClick={()=>{ history.push("/manageProfiles")}} >프로필 관리</button>      
-            </div>
-        
-        </div>
-    )
+      <ul className="choose_profile">
+        {props.data.map((a, i) => {
+          return <Profile data={props.data[i]} i={i} key={i} />;
+        })}
+      </ul>
+      <div className="profile_set">
+        <button
+          className="profile_button"
+          onClick={() => {
+            history.push("/manageProfiles");
+          }}
+        >
+          프로필 관리
+        </button>
+      </div>
+    </div>
+  );
 }
 
 function Profile(props) {
+  let history = useHistory();
 
-    let history = useHistory();
-
-    return (
-        <li className="profile">
-            <img src={"/img/profile" + (props.i + 1) + ".png"} width="130px" onClick={()=>{ history.push("/main")}}></img>
-            <div className="profile_name">{props.data.name}</div>
-        </li>
-    )
+  return (
+    <li className="profile">
+      <img
+        src={"/img/profile" + (props.i + 1) + ".png"}
+        width="130px"
+        onClick={() => {
+          history.push("/movie");
+        }}
+      ></img>
+      <div className="profile_name">{props.data.name}</div>
+    </li>
+  );
 }
 
 export default Login;
