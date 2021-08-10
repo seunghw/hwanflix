131a0cac1595839bede348463a0a8b03001a9db6
Seunghwan <ysp1307@naver.com>
2021-08-10 19:46:17 +0900

    분리작업

--- src/common/Navbar.js
+++ src/common/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-
+import { FaSearch, FaBell, FaCaretDown } from "react-icons/fa";
 function Navbar() {
   let history = useHistory();
 
@@ -8,12 +8,41 @@ function Navbar() {
     <div>
       <div className="Navbar">
         <img
-          src="img/logo.png"
+          src="/img/logo.png"
+          alt=""
           width="100px"
           onClick={() => {
             history.push("/");
           }}
         ></img>
+
+        <div
+          className="menu"
+          style={{
+            display: "flex",
+            color: "white",
+            marginLeft: "3rem",
+          }}
+        >
+          <div>홈</div>
+          <div>TV 프로그램</div>
+          <div>영화</div>
+          <div>NEW! 요즘 대세 콘텐츠</div>
+          <div>내가 찜한 콘텐츠</div>
+        </div>
+        <div
+          className="menu"
+          style={{
+            display: "flex",
+            color: "white",
+            marginLeft: "2rem",
+            textAlign: "right",
+          }}
+        >
+          <FaSearch color="white" size="20" />
+          <FaBell color="white" size="20" />
+          <FaCaretDown color="white" size="20" />
+        </div>
       </div>
     </div>
   );
